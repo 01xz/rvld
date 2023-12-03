ace715dce96fc5ed5cffc0a12e7092c22576bb86
01xz <73350430+01xz@users.noreply.github.com>
2023-12-03 19:01:13 +0800

    fix: align cast

--- src/MappedFile.zig
+++ src/MappedFile.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 const MappedFile = @This();
 
-data: []align(std.mem.page_size) u8,
+data: []u8,
 size: u64,
 
 pub fn map(path: []const u8) !MappedFile {
@@ -13,11 +13,11 @@ pub fn map(path: []const u8) !MappedFile {
     const data = try std.os.mmap(null, @intCast(size), std.os.linux.PROT.READ, std.os.linux.MAP.PRIVATE, file.handle, 0);
 
     return .{
-        .data = data,
+        .data = @alignCast(data),
         .size = size,
     };
 }
 
 pub fn unmap(self: *MappedFile) void {
-    std.os.munmap(self.data);
+    std.os.munmap(@alignCast(self.data));
 }
