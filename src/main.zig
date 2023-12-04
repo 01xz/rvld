63606af046af5592486958d6ae5c314fc5795483
01xz <73350430+01xz@users.noreply.github.com>
2023-12-04 22:21:51 +0800

    chore: clean

--- src/main.zig
+++ src/main.zig
@@ -9,34 +9,14 @@ pub fn main() !void {
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
 
-    for (args, 0..) |arg, i| {
-        std.debug.print("arg {d}: {s}\n", .{ i, arg });
+    if (args.len < 2) {
+        return error.IllegalArgs;
     }
 
-    var input_file = try Inputfile.init(args[1], std.heap.page_allocator);
-    defer input_file.deinit(std.heap.page_allocator);
+    var input_file = try Inputfile.init(args[1], allocator);
+    defer input_file.deinit(allocator);
 
     for (input_file.shdrs, 0..) |*sh_ptr, i| {
         std.debug.print("section header {d} name is {d}\n", .{ i, sh_ptr.sh_name });
     }
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
 }
