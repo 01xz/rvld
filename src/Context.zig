fdb422162e9b6b1b21c52a14d0147b560127428d
01xz <73350430+01xz@users.noreply.github.com>
2023-12-07 20:37:06 +0800

    fix: remove `arg_head`

--- src/Context.zig
+++ src/Context.zig
@@ -42,11 +42,10 @@ pub fn parseArgs(self: *Context, args: [][:0]u8) !void {
 
     var i: usize = 1;
     var arg: [:0]const u8 = undefined;
-    var arg_head = args[0];
 
     while (i < args.len) {
         if (readFlag(args[i..], "help", &i) or readFlag(args[i..], "h", &i)) {
-            std.debug.print("Usage: {s} [options] file...\n", .{arg_head});
+            std.debug.print("Usage: {s} [options] file...\n", .{args[0]});
             std.os.exit(0);
         }
 
@@ -62,7 +61,7 @@ pub fn parseArgs(self: *Context, args: [][:0]u8) !void {
             std.debug.print("output: {s}\n", .{self.context_args.output});
         } else if (try readAndParse(args[i..], "m", &i, &arg)) {
             if (!std.mem.eql(u8, arg, "elf64lriscv")) {
-                std.debug.print("{s}: unrecognised emulation mode: {s}\n", .{ arg_head, arg });
+                std.debug.print("{s}: unrecognised emulation mode: {s}\n", .{ args[0], arg });
                 std.debug.print("Supported emulations: {s}\n", .{"elf64lriscv"});
                 std.os.exit(1);
             }
@@ -85,8 +84,8 @@ pub fn parseArgs(self: *Context, args: [][:0]u8) !void {
             // ignored
         } else {
             if (args[i][0] == '-') {
-                std.debug.print("{s}: unrecognized option: {s}\n", .{ arg_head, args[i] });
-                std.debug.print("{s}: use the --help option for usage information\n", .{arg_head});
+                std.debug.print("{s}: unrecognized option: {s}\n", .{ args[0], args[i] });
+                std.debug.print("{s}: use the --help option for usage information\n", .{args[0]});
                 std.os.exit(1);
             } else {
                 try self.context_args.remained_file.append(args[i]);
